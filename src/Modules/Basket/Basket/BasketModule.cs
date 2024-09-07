72b743842b3d915e7698b07288c76c1c3a22b132
jonathangmsilva <jonathan.gmsilva@gmail.com>
2024-09-07 16:32:31 -0300

    seção 6 - dependencias

---
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Basket;
+
+public static class BasketModule
+{
+  public static IServiceCollection AddBasketModule(this IServiceCollection services,
+    IConfiguration configuration)
+  {
+    //services.AddAutoMapper(typeof(BasketModule));
+    //services.AddMediatR(typeof(BasketModule));
+    //services.AddValidatorsFromAssembly(typeof(BasketModule).Assembly);
+    //services.AddDbContext<BasketDbContext>(options =>
+    //{
+    //  options.UseSqlServer("Server=.;Initial Catalog=MyStore_Basket;Integrated Security=true");
+    //});
+
+    //services.AddScoped<IBasketRepository, BasketRepository>();
+    //services.AddScoped<IBasketService, BasketService>();
+
+    return services;
+  }
+
+
+  public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
+  {
+    //var scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+    //using var scope = scopeFactory.CreateScope();
+    //var dbContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+    //dbContext.Database.EnsureCreated();
+    return app;
+  }
+
+}
---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog;
+
+public static class CatalogModule
+{
+  public static IServiceCollection AddCatalogModule(this IServiceCollection services,
+    IConfiguration configuration)
+  {
+    //services.AddAutoMapper(typeof(CatalogModule));
+    //services.AddMediatR(typeof(CatalogModule));
+    //services.AddValidatorsFromAssembly(typeof(CatalogModule).Assembly);
+    //services.AddDbContext<CatalogDbContext>(options =>
+    //{
+    //  options.UseSqlServer("Server=.;Initial Catalog=MyStore_Catalog;Integrated Security=true");
+    //});
+
+    //services.AddScoped<IProductRepository, ProductRepository>();
+    //services.AddScoped<IProductService, ProductService>();
+
+    return services;
+  }
+
+  public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+  {
+    //var scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+    //using var scope = scopeFactory.CreateScope();
+    //var dbContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+    //dbContext.Database.EnsureCreated();
+    return app;
+  }
+
+}
